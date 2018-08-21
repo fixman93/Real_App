932202e030d84a4436ab8974143aea7f9bc19afb
Boris Civcic <civcicboris93@gmail.com>
2018-08-21 10:30:23 +0200

    add more button

--- src/containers/BurgerBuilder/BurgerBilder.js
+++ src/containers/BurgerBuilder/BurgerBilder.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react'
 
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
-import BuildControls from '../../components/Burger/BuildControls/BuildControl/BuildControl';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.4,
+    cheese: 1.3,
+    meat: 0.7
+}
 class BurgerBulder extends Component {
 
     // constructor(props) {
@@ -17,14 +23,34 @@ class BurgerBulder extends Component {
             bacon: 2,
             cheese: 1,
             meat: 1
+        },
+        totalPrice: 4
+    }
+
+    addIngredientsHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        const updatedCount = oldCount + 1
+        const updateIngredients = {
+            ...this.state.ingredients
         }
+        updateIngredients[type] = updatedCount
+        const priceAddition = INGREDIENT_PRICES[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceAddition
+        this.setState({totalPrice: newPrice, ingredients: updateIngredients})
+    }
+
+    removeIngredientsHandler = (type) => {
+        
     }
 
     render() {
         return (
             <Aux>
                 <Burger ingredients = {this.state.ingredients} />
-                <BuildControls />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientsHandler}
+                />
             </Aux>
         )
     }
