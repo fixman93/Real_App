6b9823e0653d5cc131076cda18b1732805a45ad7
Boris Civcic <civcicboris93@gmail.com>
2018-08-21 08:43:28 +0200

    Burger structure added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Layout from './components/Layout/Layout'
+import BurgerBilder from './containers/BurgerBuilder/BurgerBilder'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Layout>
+          <BurgerBilder />
+        </Layout>
       </div>
     );
   }
