a4a05cf1c9c1bdc99237545d5a5c9db87c19f125
Boris Civcic <civcicboris93@gmail.com>
2018-08-21 15:54:06 +0200

    backdrop added to the modal

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -25,8 +25,7 @@ const buildControls = (props) => (
         className={classes.OrderButton}
         disabled={!props.purchasable}
         onClick={props.ordered}
-        >
-        ORDER NOW</button>
+        >ORDER NOW</button>
     </div>
 )
 
