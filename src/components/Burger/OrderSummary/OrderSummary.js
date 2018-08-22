0b29021c3040eba1a27c73255194aff16c48ac78
Boris Civcic <civcicboris93@gmail.com>
2018-08-22 13:01:05 +0200

    Order Summary updated

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,7 +16,9 @@ const orderSummary = (props) => {
         <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients</p>
-            <ul>{ingredientSummary}</ul>
+            <ul>
+                {ingredientSummary}
+            </ul>
             <p>Continue to checkout</p>
             <button>CANCEL</button>
             <button>CONTINUE</button>
