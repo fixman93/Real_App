939e6e69443d809b7d26ba0b91aad17aaadb8c7a
Boris Civcic <civcicboris93@gmail.com>
2018-08-23 11:23:30 +0200

    backdrop added to for the mobile design

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component} from 'react'
 import Aux from '../../hoc/Aux'
 
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDraw from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDraw />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends Component {
+    state = {
+        showSideDraw: true
+    }
+    sideDrawCloseHandler = () => {
+        this.setState({showSideDraw: false})
+    }
 
-export default layout
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDraw 
+                open={this.state.showSideDraw} 
+                closed={this.sideDrawCloseHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,26 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import Style from './SideDraw.css'
+import BackDrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
 
 const sideDraw = (props) => {
+    let attachedClasses = [Style.SideDraw, Style.Close]
+    if (props.open) {
+        attachedClasses = [Style.SideDraw, Style.Open]
+    }
     return (
-        <div className={Style.SideDraw}>
-            <div className={Style.Logo}>
-                <Logo />
+        <Aux>
+            <BackDrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join( ' ' )}>
+                <div className={Style.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     )
 }
 
