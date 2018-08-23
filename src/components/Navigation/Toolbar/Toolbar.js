b01bb88a85c40b66d34eec5ef0ef730e6617d7c9
Boris Civcic <civcicboris93@gmail.com>
2018-08-23 09:46:52 +0200

    navbar menu added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import Logo from '../../Logo/Logo'
 import Style from './Toolbar.css'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = () => (
     <header className={Style.Toolbar}>
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 )
