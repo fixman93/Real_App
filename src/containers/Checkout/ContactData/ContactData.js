e069ab5920d80f3156b9b36d5269e8f4d8b73bec
Boris Civcic <civcicboris93@gmail.com>
2018-09-10 08:48:51 +0200

    file structure updated and action creator added to the project

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -109,7 +109,7 @@ class ContactData extends Component {
             price: this.props.price,
             orderData: formData
         }
-        axios.post( '/orders.json', order )
+        axios.post( '/order.json', order )
             .then( response => {
                 this.setState( { loading: false } );
                 this.props.history.push( '/' );
---
+++ src/store/actions/index.js
@@ -0,0 +1,5 @@
+export { 
+    addIngredient, 
+    removeIngredient
+} from './burderBuilder'
+export {} from './order'
